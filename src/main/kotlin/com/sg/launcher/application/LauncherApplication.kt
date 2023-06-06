f356eff97228bca27d6266b2e307aa9f753ff816
Sachin Goyal <sachin.goyal@multiverse.io>
2023-06-06 23:16:56 +0100

    spring boot open api support

--- src/main/kotlin/com/sg/launcher/application/LauncherApplication.kt
+++ src/main/kotlin/com/sg/launcher/application/LauncherApplication.kt
@@ -1,9 +1,15 @@
 package com.sg.launcher.application
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@EnableJpaRepositories("com.sg.launcher.repository")
+@EntityScan("com.sg.launcher.repository.entity")
+@ComponentScan("com.sg.launcher")
 class LauncherApplication
 
 fun main(args: Array<String>) {
